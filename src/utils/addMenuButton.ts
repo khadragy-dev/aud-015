c7285618029f58219dc0c1019064cd3894ffb65b
seif-hassan98 <seif.hassan.abbas@gmail.com>
2023-12-20 11:06:48 +0200

    Add menu button functionality and dropdown toggle

--- src/utils/addMenuButton.ts
+++ src/utils/addMenuButton.ts
@@ -1,20 +1,30 @@
 const addButton = () => {
-  const buttonWrapper = document.querySelector(".site-header__icons-wrapper");
+  const buttonWrapper = document.querySelector('.site-header__icons-wrapper');
   if (buttonWrapper) {
-    buttonWrapper.insertAdjacentHTML(
-      "beforeend",
-      `<div class="ca_toggle_button"></div>`
-    );
+    buttonWrapper.insertAdjacentHTML('beforeend', `<div class="ca_toggle_button"></div>`);
   }
 };
 
 const toggleMenu = () => {
-  const button = document.querySelector(".ca_toggle_button");
+  const button = document.querySelector('.ca_toggle_button');
   if (button) {
-    button.addEventListener("click", function () {
+    button.addEventListener('click', function () {
       const body = document.body;
       if (body) {
-        body.classList.toggle("ca_active_menu");
+        body.classList.toggle('ca_active_menu');
+      }
+    });
+  }
+};
+
+const toggleDropdown = () => {
+  const button = document.querySelector('.ca_dropdown_toggle_button');
+  const buttonParent = document.querySelector('.ca_dropdown_toggle_elements-wrapper');
+  if (button && buttonParent) {
+    button.addEventListener('click', function () {
+      const body = document.body;
+      if (body) {
+        buttonParent.classList.toggle('ca_active_dropdown');
       }
     });
   }
@@ -23,4 +33,5 @@ const toggleMenu = () => {
 export function addButtonFunctionality() {
   addButton();
   toggleMenu();
+  toggleDropdown();
 }
